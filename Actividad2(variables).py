cba0248f2c88b3f21d2d5e8c69473b03abd83828
e19197@ism.edu.ec <e19197@ism.edu.ec>
2026-04-14 20:55:19 -0500

    actividad2

---
+++ Actividad2(variables).py
@@ -0,0 +1,22 @@
+print(len("Mishelle Pacheco"))
+
+# %%  
+first_name= "Mishelle"
+longitud= len(first_name)
+print("First name lenght:",longitud)
+
+# %%
+#Segunda forma de hacer el ejercicio
+first_name= "Mishelle"
+print("First name lenght",len(first_name))
+
+# %% 
+print(type(first_name))
+print(type(longitud))
+# %%
+
+#Multiples variables en una sola línea
+primer_nombre, apellido, pais, edad, estado_civil= "Mishelle","Pacheco","Ecuador","16 años","Soltera"
+
+print(primer_nombre,pais)
+# %%
