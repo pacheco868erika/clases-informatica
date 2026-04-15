c52a98f4b1eab949ed521fe156cbfc57c2b23a5f
Mishelle Pacheco <e19197@ism.edu.ec>
2026-04-15 09:09:10 -0500

    actividad2.2

---
+++ Actividad2(variables).py
@@ -0,0 +1,94 @@
+# Comentario de múltiples líneas
+"""nombre: Mishelle
+fecha: 15/4/2026
+titulo: Actividad2"""
+
+# %% 
+#Variales 
+nombre= "Mishelle"
+apellido= "Pacheco"
+nombreCompleto= "Mishelle Pacheco"
+pais= "Ecuador"
+ciudad= "Quito"
+edad= "16"
+anio= "2026"
+estaCasada= "No"
+esVerdadero= "Sí"
+luzEncendida= "Sí"
+#Variables en una sla línea
+hobby,serie,cumpleanos= "Pintar","strange things","el 5 de junio"
+
+
+# %%
+#Nivel2 
+
+#Comprobar type 
+print(type(nombre)) 
+print(type(apellido))
+print(type(nombreCompleto))
+print(type(pais))
+print(type(ciudad))
+print(type(edad))
+print(type(anio))
+print(type(estaCasada))
+print(type(esVerdadero))
+print(type(luzEncendida))
+print(type(hobby))
+print(type(serie))
+print(type(cumpleanos))
+
+# %%
+longitud=len(nombre)
+#Compara la longitud de la variable nombre y tu variable pellido
+print(len(nombre) > len(apellido))
+print(len(nombre) < len(apellido))
+print(len(nombre) == len(apellido))
+
+# %%
+numeroUno= 5
+numeroDos= 4
+total=numeroUno+numeroDos
+print(total)
+
+diferenecia= numeroUno-numeroDos
+print(diferenecia)
+
+producto=numeroUno*numeroDos
+print(producto)
+
+division= numeroUno/numeroDos
+print(division)
+
+residuo= numeroDos%numeroUno
+print(residuo)
+
+potencia= numeroUno**numeroDos
+print(potencia)
+
+divisionEntera= numeroUno//numeroDos
+print(divisionEntera)
+
+# %%
+#El area del circulo
+radio= 30
+areaCirculo= 3.14*radio**2
+print(areaCirculo)
+
+circuferenciaCirculo= 2*3.14*areaCirculo
+print(circuferenciaCirculo)
+
+radi0=float(input("Ingresa el valor del radio")
+area=3.14*radio0**2
+print(area)
+
+nombre = str(input("Ingrese su nombre: "))
+ 
+apellido = str(input("Ingrese su apellido: "))
+ 
+pais = str(input("Ingrese su país: "))
+ 
+edad = int(input("Ingrese su edad: "))
+
+help("keywords")
+
+
