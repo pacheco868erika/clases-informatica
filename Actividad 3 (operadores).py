5b92aafedaf43f131c6bdec86a9dbd1554f121af
Mishelle Pacheco <e19197@ism.edu.ec>
2026-04-16 14:13:12 -0500

    actividad 3

---
+++ Actividad 3 (operadores).py
@@ -0,0 +1,44 @@
+#Operadores
+"""Nombre: Mishelle Pacheco
+Fecha: 16/04/2026"""
+# %%
+# ariables
+edad= 16 
+estatura= 1.50
+
+# %%
+# base y altura de un triángulo 
+base= float(input("Ingresa la base de un triángulo: "))
+altura= float(input("Ingresa la altura un triángulo: "))
+area= 0.5 * base * altura
+print("Área: ", area)
+
+# %%
+#Lados de un triángulo y su perímetro
+ladoA= float(input("Ingresa el lado A de un triángulo: "))
+ladoB= float(input("Ingresa el lado B de un triángulo: "))
+ladoC= float(input("Ingresa el lado C de un triángulo: "))
+perimetro = ladoA + ladoB + ladoC
+print("Perímetro: ", perimetro)
+
+# %%
+#la longitud y el ancho de un rectángulo
+longitud= float(input("Ingresa la longitud de un rectángulo: "))
+ancho= float(input("Ingresa la longitud de un ancho: ")) 
+area= longitud * ancho 
+perimetro = 2 * (longitud + ancho) 
+print("Área:", area) 
+print("Perímetro:", perimetro)
+
+# %%
+#área y la circunferencia de un círculo 
+radio = float(input("Ingrese el radio: ")) 
+pi = 3.14 
+area = pi * radio * radio 
+circunferencia = 2 * pi * radio 
+print("Área:", area) 
+print("Circunferencia:", circunferencia) 
+
+# %%
+#la pendiente y la distancia euclidiana entre los puntos
+  
