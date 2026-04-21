02b0604e3b01ee015d99e4815377cde6cafdb4cf
Mishelle Pacheco <e19197@ism.edu.ec>
2026-04-21 10:57:58 -0500

    actividad en clase

---
+++ actividad 4 (strings).py
@@ -0,0 +1,20 @@
+stringVariasLineas= """
+Mishelle Pacheco
+Informática
+"""
+print(stringVariasLineas)
+colegio= "ISM"
+longitud= len(colegio)
+print(longitud)
+print(len(colegio))
+print(len("San Francisco de Quito"))
+nombre= "Mishelle"
+apellido= "Pacheco"
+nombreCompleto= nombre + " " + apellido + " "
+print(nombreCompleto)
+print("Mi nombre completo es",nombre,apellido)
+print(nombreCompleto*5)
+print("python\nchallenge")
+print("nombre\tsemana1\tsemana2\tsemanan3")
+print("símbolo(\\)")
+print(f"Mi nombre es {nombre} y mi apellido {apellido}")
