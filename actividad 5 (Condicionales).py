6e2565be2f3bb042eb10a76c74718b756a9220a8
Mishelle Pacheco <e19197@ism.edu.ec>
2026-04-23 14:16:46 -0500

    actividad

---
+++ actividad 5 (Condicionales).py
@@ -0,0 +1,17 @@
+# %%
+# Ejercicio 
+calificacion = float(input("Ingrese su calificación (1-100): "))
+if calificacion >= 90:
+     print("Su calificación es A")
+else:
+    if calificacion >= 80:
+        print("Su calificación es B")
+    else:
+        if calificacion >= 70:
+            print("Su calificación es C")
+        else:
+            print("Su calificación es D")
+
+
+
+
