be6cdd6ae079811ec0a090a4202536083b5b9926
Mishelle Pacheco <e19197@ism.edu.ec>
2026-05-13 08:54:36 -0500

    for

--- actividad 6 (ciclo while).py
+++ actividad 6 (ciclo while).py
@@ -1,16 +1,41 @@
-opcion = ""
-while opcion != "C":
-    print ("A. Saludar")
-    print ("B. Mostrar el mensaje")
-    print ("C. Salir")
 
-    opcion = input("Ingrese un opción: ")
-    print (opcion)
-    if opcion == "A":
-        print ("Bienvenido")
-    elif opcion == "B":
-        print ("Estamos aprendiendo el ciclo while")
-    elif opcion == "C":
-        print ("Salir")
+# %%
+palabra = input ("Ingrese una palabra: ")
+consonante = 0
+vocales = 0
+for letra in palabra.lower():
+    if letra == "a" or letra == "e" or letra == "i" or letra =="o" or letra == "u":
+        vocales = vocales + 1
     else:
-        print ("Opción no válida")
+        consonante= consonante + 1 
+print (len(palabra))
+print (f"Tiene este número de vocales: {vocales} y de consonantes {consonante}")
+# %%
+
+# for en un Set 
+# %%
+companias = {"Facebook", "Facebook",  "Google", "Apple", "Amazon"}
+for compania in companias:
+    print (compania)
+
+# Break 
+# %%
+lista = [1, 2, 3, 4, 5]
+numeros = int(input("Ingresa un número: "))
+for numero in lista:
+    if numero == numeros:
+        print ("Número encontrado")
+        break 
+else:
+    print ("Numero no encontrado")
+
+# Continue 
+# %%
+cedula = input("Ingrese su número de cedula: ")
+cedula_limpia = ""
+for caracter in cedula:
+    if caracter == "-" or caracter == " ":
+        continue
+    cedula_limpia = cedula_limpia + caracter
+print (cedula_limpia)
+# %%
