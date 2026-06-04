0516cad0cde258dbe0da8b8fd19973a4a4fb8e92
msihelle_pacheco <e19197@ism.edu.ec>
2026-06-04 13:55:20 -0500

    Update and rename actividad 7.py to actividad 7 (Ciclo for).py

---
+++ actividad 7 (Ciclo for).py
@@ -0,0 +1,52 @@
+#Ejercicios de listas 
+notas = [8.5, 6.0, 9.0, 7.0, 5.5] 
+suma = 0 
+aprobaron = 0
+reprobaron = 0
+for calificacion in notas:
+    suma = suma + calificacion
+    if calificacion >= 7: 
+     aprobaron + 1
+    else:
+       reprobaron + 1
+promedio =  suma / len(notas)
+
+# Ciclo for %%
+palabra = input ("Ingrese una palabra: ")
+consonante = 0
+vocales = 0
+for letra in palabra.lower():
+    if letra == "a" or letra == "e" or letra == "i" or letra =="o" or letra == "u":
+        vocales = vocales + 1
+    else:
+        consonante= consonante + 1 
+print (len(palabra))
+print (f"Tiene este número de vocales: {vocales} y de consonantes {consonante}")
+# %%
+
+# for en un Set 
+# %%
+companias = {"Facebook", "Facebook",  "Google", "Apple", "Amazon"}
+for compania in companias:
+    print (compania)
+
+# Break 
+# %%
+lista = [1, 2, 3, 4, 5]
+numeros = int(input("Ingresa un número: "))
+for numero in lista:
+    if numero == numeros:
+        print ("Número encontrado")
+        break 
+else:
+    print ("Numero no encontrado")
+
+# Continue 
+# %%
+cedula = input("Ingrese su número de cedula: ")
+cedula_limpia = ""
+for caracter in cedula:
+    if caracter == "-" or caracter == " ":
+        continue
+    cedula_limpia = cedula_limpia + caracter
+print (cedula_limpia)
